846afbca09a3322d7d3ab8a9235ce3a316f6dd83
elianosvaldo23 <elianverdeciamesa@gmail.com>
2025-07-30 10:48:27 -0400

    Update telegram_admin_bot.py

--- scripts/telegram_admin_bot.py
+++ scripts/telegram_admin_bot.py
@@ -11,7 +11,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command, Text
+from aiogram.filters import Command
+from aiogram.filters.text import Text
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
